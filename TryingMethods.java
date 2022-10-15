b6da78e22f6ab7a7808314a68171d4d5e7a14f74
Emmanuel Enya <emmanuelenya@meltwater.org>
2022-10-15 10:58:32 +0000

    initial project set up

---
+++ TryingMethods.java
@@ -0,0 +1,12 @@
+/**
+ * TryingMethods is a playground for practicing methods usage in Java 
+ */
+public class TryingMethods {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
